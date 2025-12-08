eb338c09b7c2b052e51d893c88d8cca3019681d0
Pauloh349 <muiruripaul3492@gmail.com>
2025-12-08 21:06:12 +0300

    second

--- lostfound/urls.py
+++ lostfound/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns =[
     path ('',views.home,name='home'),
-    path('lostItems/',views.lostItems, name = 'lostItems'),
+    path('lost_items/',views.lostItems, name = 'lostItems'),
     path("found/", views.found, name="found"),
     path("how_it_works/", views.howItWorks, name = "howItWorks"),
     path("success_stories/", views.successStories, name = "successStories"),
