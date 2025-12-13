e96334268af27c504a7941851e97db31f36bb5bd
Pauloh349 <muiruripaul3492@gmail.com>
2025-12-13 21:02:35 +0300

    Configure PostgreSQL for Render

--- reclaima_project/settings.py
+++ reclaima_project/settings.py
@@ -82,10 +82,26 @@ WSGI_APPLICATION = 'reclaima_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': (
+            'django.db.backends.postgresql'
+            if os.environ.get('DB_HOST')
+            else 'django.db.backends.sqlite3'
+        ),
+        'NAME': os.environ.get('DB_NAME', BASE_DIR / 'db.sqlite3'),
+        'USER': os.environ.get('DB_USER', ''),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', ''),
+        'PORT': os.environ.get('DB_PORT', ''),
     }
 }
 
